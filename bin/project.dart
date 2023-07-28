4160aa47100282413fa6b2ac96955457beb906dd
SiroProg <070777sss@gmail.com>
2023-07-28 10:39:20 +0500

    test project in main

--- bin/project.dart
+++ bin/project.dart
@@ -1,3 +1,4 @@
-void main(List<String> args) {
-  
-}
+import 'package:project/src/pages/intro_page.dart';
+import 'package:project/src/service/io_service.dart';
+
+void main() async => IntroPage(io: IOService()).start();
